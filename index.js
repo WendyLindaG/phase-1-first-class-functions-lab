aaba6a52a3c43556a2f6e007df2126c0bf428450
Wendy Linda Gakii <wendylinda2017@gmail.com>
2023-03-19 01:19:47 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+function returnFirstTwoDrivers() {
+    let firstTwoDrivers;
+    firstTwoDrivers = drivers.slice(0, 2);
+    return firstTwoDrivers;
+}
+function returnLastTwoDrivers() {
+    let lastTwoDrivers;
+    lastTwoDrivers = drivers.slice(-2)
+    return lastTwoDrivers;
+}
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+function createFareMultiplier(multiplier) {
+    return function(value) {
+        return value * multiplier;
+    }
+}
+function fareDoubler(fare) {
+    let price = 10;
+    fare = price * 2;
+    return fare;
+}
+function fareTripler(fare) {
+    let price = 12;
+    fare = price  * 3;
+    return fare;
+}
+function selectDifferentDrivers(drivers, driverSelectFunction) {
+    return driverSelectFunction(returnFirstTwoDrivers);
+}
+function selectDifferentDrivers(drivers, driverSelectFunction) {
+    return driverSelectFunction(returnLastTwoDrivers); 
+}
